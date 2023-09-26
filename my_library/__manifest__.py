eeead16846f95abb8b2d9ea2b9ae2bfa93ce88a1
marion586 <marionmenye@gmail.com>
2023-09-26 17:02:09 +0300

    first commit

---
+++ my_library/__manifest__.py
@@ -0,0 +1,12 @@
+{
+ 'name': "My library",
+ 'summary': "Manage books easily",
+ 'description': """Description related to library.""",
+ 'author': "Your name",
+ 'website': "http://www.example.com",
+ 'category': 'Uncategorized',
+ 'version': '13.0.1',
+ 'depends': ['base'],
+'data': ['views/library_book.xml'],
+ 'demo': [],
+} 
